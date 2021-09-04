89627166baaec35db533b85580f0372e5ae464c1
Tan Yuanhong <leotan.yh@gmail.com>
2021-09-04 14:24:36 +0800

    Handle local submission.json

--- aivle-worker/__main__.py
+++ aivle-worker/__main__.py
@@ -1,8 +1,12 @@
 import settings
-import virtualenv
+import aivle_venv
 import sandbox
+import client
 
-print(settings.TEMP_VENV_FOLDER)
-env_name = virtualenv.create_venv("/home/leo/aivle-runner-v2/aivle-worker/requirements/base.txt")
-print(env_name)
-sandbox.run_with_venv(env_name, ["bash", "./bootstrap.sh"], "/tmp/aivle-worker/grading/test")
+# print(settings.TEMP_VENV_FOLDER)
+# env_name = aivle_venv.create_venv("/home/leo/aivle-runner-v2/aivle-worker/requirements/base.txt")
+# print(env_name)
+# sandbox.run_with_venv(env_name, ["bash", "./bootstrap.sh"], "/tmp/aivle-worker/grading/test")
+
+submission = client.get_submission()
+client.run_submission(submission)
--- aivle-worker/client.py
+++ aivle-worker/client.py
@@ -1,6 +1,34 @@
 import json
+import os
+import shutil
+
+import aivle_venv
+import sandbox
+import settings
+
+
+class Submission:
+    def __init__(self, id: str, env: dict, grader: str, agent: str, bootstrap: str):
+        self.id = id
+        self.env = env
+        self.grader = grader
+        self.agent = agent
+        self.bootstrap = bootstrap
 
 
 def get_submission():
     with open("/home/leo/aivle-runner-v2/submission.json", "r") as f:
-        return json.loads(f.read())
+        obj = json.loads(f.read())
+        return Submission(**obj)
+
+
+def run_submission(s: Submission):
+    env_name = aivle_venv.create_venv(s.env["requirements"])
+    temp_grading_folder = os.path.join(settings.TEMP_GRADING_FOLDER, s.id)
+    if not os.path.exists(temp_grading_folder):
+        os.mkdir(temp_grading_folder)
+    shutil.copy(s.agent, temp_grading_folder)
+    shutil.copy(s.grader, temp_grading_folder)
+    shutil.copy(s.bootstrap, temp_grading_folder)
+    sandbox.run_with_venv(env_name, ["bash", "./bootstrap.sh"], temp_grading_folder)
+    shutil.rmtree(temp_grading_folder)
